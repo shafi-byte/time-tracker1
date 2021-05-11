1fee079c2c8677bb78286f3098e87ee2e988e878
Utkarsh <utkarsh30@yahoo.in>
2021-05-11 08:26:24 +0530

    Adding SCM

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,11 +1,11 @@
 package training.taylor.timetracker.core;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import training.taylor.timetracker.core.dao.TimeEntry;
 
-import java.util.List;
+// import java.util.List;
 
 @Component
 public class Tracker {
